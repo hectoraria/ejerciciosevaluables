a3bc712cee71ad901ce090fef439d1453e4115dc
hectoraria <145675493+hectoraria@users.noreply.github.com>
2023-10-02 13:58:48 +0200

    Update Ejercicio5.java

--- EjerciciosEvaluables-Tema1/src/ejerciciosevaluables/Ejercicio5.java
+++ EjerciciosEvaluables-Tema1/src/ejerciciosevaluables/Ejercicio5.java
@@ -33,7 +33,7 @@ public class Ejercicio5 {
 		System.out.println("Introduce el dato x: ");
 		//Recoge el dato de la x con el escaner
 		x = sc.nextFloat();
-		//Defino la funcion que tiene area
+		//Defino la funcion para que calcule y
 		y= (float) (a*Math.pow(x,2)) + (b*x) + c;
 		//Imprimo el resultado de la y
 		System.out.println("El resultado de y es: " + y );
